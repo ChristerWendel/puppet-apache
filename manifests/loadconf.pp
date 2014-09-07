3998cd0ebc65ef8d023a8597f646a23eb647bc4f
Christer Wendel <christer.wendel@altrusoft.se>
2014-09-07 15:21:08 +0200

    Fixed a typo

--- manifests/loadconf.pp
+++ manifests/loadconf.pp
@@ -8,7 +8,7 @@
 
 define apache::loadconf() {
   exec { "/usr/sbin/a2enconf ${name}" :
-    creates => "/etc/apache2/conf.enabled/${name}.conf",
+    creates => "/etc/apache2/conf-enabled/${name}.conf",
     notify  => Service['apache2'],
   }
 }
--- spec/defines/loadconf_spec.rb
+++ spec/defines/loadconf_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'apache::loadconf', :type => :define do
   let(:title) { 'config' }
   it { should contain_exec('/usr/sbin/a2enconf config')
-      .with_creates('/etc/apache2/conf.enabled/config.conf')
+      .with_creates('/etc/apache2/conf-enabled/config.conf')
   }
 end
